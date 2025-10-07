dde3769e41ef0acf42a61ba872caf250dad7a198
koto <kotonitro554@gmail.com>
2025-10-07 17:19:10 -0300

    ayudadiosito

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -1,4 +1,7 @@
 import { handleSuccess } from "../Handlers/responseHandlers.js";
+import { userRepository } from "../services/user.service.js";
+import { userValidation } from "../validations/user.validation.js";
+import bcrypt from "bcrypt";
 
 export function getPublicProfile(req, res) {
   handleSuccess(res, 200, "Perfil público obtenido exitosamente", {
@@ -15,13 +18,23 @@ export function getPrivateProfile(req, res) {
   });
 }
 
-import { userRepository } from "../services/user.service.js";
-import bcrypt from "bcrypt";
 
 export async function updateProfile(req, res) {
   try {
     const userId = req.user.sub;
     const { email, password } = req.body;
+
+    // Validar solo los campos presentes
+    const { error } = userValidation.validate(
+      { email, password },
+      { presence: "optional" }
+    );
+    if (error) {
+      return res
+        .status(400)
+        .json({ message: "Datos inválidos", details: error.message });
+    }
+
     const user = await userRepository.findOneBy({ id: userId });
     if (!user) {
       return res.status(404).json({ message: "Usuario no encontrado" });
